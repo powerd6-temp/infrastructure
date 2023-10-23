b2500c3b3480468dc523cf6c49a0db5e1b38d660
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-10-23 11:14:26 +0200

    move files to new src

--- src/repositories.ts
+++ src/repositories.ts
@@ -2,7 +2,7 @@ import * as github from "@pulumi/github";
 import * as pulumi from "@pulumi/pulumi";
 import { organization } from "./organizations";
 import { slugify } from "./helpers/slugify";
-import { licenseFileContent, contributingFileContent } from "./repositoryFiles";
+import { getRepositoryFiles } from "./repositoryFiles";
 
 const bypassesUsers = ["/HectorCastelli"];
 
@@ -239,43 +239,4 @@ export const repositories = repoConfigurations.map((r) => {
   };
 });
 
-function getRepositoryFiles(r: github.RepositoryArgs & { name: string; }, repo, mainBranch, mainBranchProtection) {
-  const licenseFile = new github.RepositoryFile(
-    `${r.name}/Files/License`,
-    {
-      repository: repo.name,
-      branch: mainBranch.branch,
-      file: "LICENSE.md",
-      content: licenseFileContent,
-      commitAuthor: "powerd6/infrastructure",
-      commitEmail: "infrastructure@powerd6.org",
-      commitMessage: "Updating LICENSE.md . Managed by infrastructure.",
-      overwriteOnCreate: true,
-    },
-    {
-      dependsOn: [mainBranch, mainBranchProtection],
-      parent: repo,
-    }
-  );
-
-  const contributingFile = new github.RepositoryFile(
-    `${r.name}/Files/Contributing`,
-    {
-      repository: repo.name,
-      branch: mainBranch.branch,
-      file: "CONTRIBUTING.md",
-      content: contributingFileContent,
-      commitAuthor: "powerd6/infrastructure",
-      commitEmail: "infrastructure@powerd6.org",
-      commitMessage: "Updating CONTRIBUTING.md . Managed by infrastructure.",
-      overwriteOnCreate: true,
-    },
-    {
-      dependsOn: [mainBranch, mainBranchProtection],
-      parent: repo,
-      deletedWith: repo
-    }
-  );
-  return { licenseFile, contributingFile };
-}
 
--- src/repositoryFiles.ts
+++ src/repositoryFiles.ts
@@ -1,5 +1,8 @@
+import * as github from "@pulumi/github";
 import { readFileSync } from "fs";
 import { resolve } from "path";
+import { licenseFileContent, contributingFileContent } from "./repositoryFiles";
+import { licenseFileContent, contributingFileContent } from "./repositoryFiles";
 
 export const licenseFileContent = readFileSync(
   resolve(__dirname, "../content/LICENSE.md"),
@@ -8,4 +11,43 @@ export const licenseFileContent = readFileSync(
 export const contributingFileContent = readFileSync(
   resolve(__dirname, "../content/CONTRIBUTING.md"),
   "utf-8"
-);
+);export function getRepositoryFiles(r: github.RepositoryArgs & { name: string; }, repo, mainBranch, mainBranchProtection) {
+  const licenseFile = new github.RepositoryFile(
+    `${r.name}/Files/License`,
+    {
+      repository: repo.name,
+      branch: mainBranch.branch,
+      file: "LICENSE.md",
+      content: licenseFileContent,
+      commitAuthor: "powerd6/infrastructure",
+      commitEmail: "infrastructure@powerd6.org",
+      commitMessage: "Updating LICENSE.md . Managed by infrastructure.",
+      overwriteOnCreate: true,
+    },
+    {
+      dependsOn: [mainBranch, mainBranchProtection],
+      parent: repo,
+    }
+  );
+
+  const contributingFile = new github.RepositoryFile(
+    `${r.name}/Files/Contributing`,
+    {
+      repository: repo.name,
+      branch: mainBranch.branch,
+      file: "CONTRIBUTING.md",
+      content: contributingFileContent,
+      commitAuthor: "powerd6/infrastructure",
+      commitEmail: "infrastructure@powerd6.org",
+      commitMessage: "Updating CONTRIBUTING.md . Managed by infrastructure.",
+      overwriteOnCreate: true,
+    },
+    {
+      dependsOn: [mainBranch, mainBranchProtection],
+      parent: repo,
+      deletedWith: repo
+    }
+  );
+  return { licenseFile, contributingFile };
+}
+
